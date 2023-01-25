a7c2dac914d073557cbf192c627546a52bd20eda
MLB-95 <miguel.lb95@gmail.com>
2023-01-25 11:20:24 +0100

    Create Optional (join multiple tables).sql

---
+++ LABS/week3/Optional (join multiple tables).sql
@@ -0,0 +1,6 @@
+use sakila;
+# 1. Write a query to display for each store its store ID, city, and country.
+
+# 2. Write a query to display how much business, in dollars, each store brought in.
+
+# 
