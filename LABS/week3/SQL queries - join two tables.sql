a367cebab88d04f857fdc48ce48e686c996dd90b
MLB-95 <miguel.lb95@gmail.com>
2023-01-25 10:12:03 +0100

    Update SQL queries - join two tables.sql

--- LABS/week3/SQL queries - join two tables.sql
+++ LABS/week3/SQL queries - join two tables.sql
@@ -3,16 +3,18 @@ use sakila;
 # 1. Which actor has appeared in the most films?
 select * from actor;
 select * from film_actor;
-select first_name, last_name, count(actor_id) from actor
+select distinct first_name, last_name, count(actor_id) from actor
 inner join film_actor using (actor_id) 
-group by actor_id order by count(actor_id) desc;
+group by actor_id order by count(actor_id) desc
+limit 1;
 
 # 2. Most active customer (the customer that has rented the most number of films)
 select * from customer;
 select * from rental;
 select first_name, last_name, count(customer_id) from customer
 inner join rental using (customer_id)
-group by first_name, last_name order by count(customer_id) desc;
+group by first_name, last_name order by count(customer_id) desc
+limit 1;
 
 # 3. List number of films per category.
 select * from category;
@@ -35,6 +37,11 @@ inner join language using(language_id)
 where name = "English" and title regexp "^M.*"
 or "Italian" and title regexp "^M.*";
 
+select title, name from film
+inner join language using (language_id)
+where language.name = 'English' and 'Italian'
+and film.title like "M%";
+
 # 6. Display the total amount rung up by each staff member in August of 2005.
 select * from payment;
 select first_name, last_name, sum(amount) from staff
@@ -45,14 +52,16 @@ group by first_name, last_name;
 # 7. List each film and the number of actors who are listed for that film.
 select * from film;
 select * from film_actor;
+
 select title, count(actor_id) from film
-inner join film_actor using (film_id)
+left join film_actor using (film_id)
 group by title
 order by count(actor_id) desc;
 
 # 8. Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name.
 select * from payment;
 select * from customer;
+
 select first_name, last_name, sum(amount) from customer
 inner join payment using (customer_id)
 group by first_name, last_name
@@ -62,6 +71,7 @@ order by last_name;
 select * from film;
 select * from actor;
 select * from film_actor;
+
 select first_name, last_name, film_id from actor
 left join film_actor using(actor_id)
 where film_id is null;
@@ -73,7 +83,7 @@ select * from customer;
 select address, customer_id from address
 left join customer using(address_id)
 where customer_id is null
-and address;
+and address like"%e";
 
 # Optional: what is the most rented film?
 
@@ -85,5 +95,6 @@ select title, count(rental_id) from film
 inner join inventory using(film_id)
 inner join rental using(inventory_id)
 group by title
-order by count(rental_id) desc;
+order by count(rental_id) desc
+limit 1;
 
