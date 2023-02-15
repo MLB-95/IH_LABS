b40ad2e72f64476f0d78b71838130856eb1e450d
MLB-95 <miguel.lb95@gmail.com>
2023-02-15 11:08:13 +0100

    .

--- LABS/week6/config.py
+++ LABS/week6/config.py
@@ -1,2 +1,2 @@
-client_id= b95275128a104b84aeadfa666320994b
-secret_id= c3d83d482b6541efabb0c2c550e1edde
+client_id= "b95275128a104b84aeadfa666320994b"
+client_secret= "c3d83d482b6541efabb0c2c550e1edde"
